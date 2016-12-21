55e4218fbc48096b223654a066293a5aaeaf60e6
Yongzhi <yongzhi.yang@tubemogul.com>
2016-12-21 13:36:40 +0800

    initial time-calculation project

---
+++ src/main/java/com/troy/young/App.java
@@ -0,0 +1,13 @@
+package com.troy.young;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
